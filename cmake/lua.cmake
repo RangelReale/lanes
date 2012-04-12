dedfecd90d73f85df590453aec02fd3d9ad5ece9
David Manura <dm.git@math2.org>
2012-04-11 23:44:25 -0400

    cmake: update dist.cmake

--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -1,84 +1,21 @@
-# LuaDist CMake utility library.
-# Provides variables and utility functions common to LuaDist CMake builds.
+# LuaDist CMake utility library for Lua.
 # 
-# Copyright (C) 2007-2011 LuaDist.
+# Copyright (C) 2007-2012 LuaDist.
 # by David Manura, Peter Drahos
 # Redistribution and use of this file is allowed according to the terms of the MIT license.
 # For details see the COPYRIGHT file distributed with LuaDist.
 # Please note that the package source code is licensed under its own license.
 
-## INSTALL DEFAULTS (Relative to CMAKE_INSTALL_PREFIX)
-# Primary paths
-set ( INSTALL_BIN bin CACHE PATH "Where to install binaries to." )
-set ( INSTALL_LIB lib CACHE PATH "Where to install libraries to." )
-set ( INSTALL_INC include CACHE PATH "Where to install headers to." )
-set ( INSTALL_ETC etc CACHE PATH "Where to store configuration files" )
 set ( INSTALL_LMOD ${INSTALL_LIB}/lua CACHE PATH "Directory to install Lua modules." )
 set ( INSTALL_CMOD ${INSTALL_LIB}/lua CACHE PATH "Directory to install Lua binary modules." )
-set ( INSTALL_SHARE share CACHE PATH "Directory for shared data." )
 
-# Secondary paths
-set ( INSTALL_DATA ${INSTALL_SHARE}/${PROJECT_NAME} CACHE PATH "Directory the package can store documentation, tests or other data in.")
-set ( INSTALL_DOC ${INSTALL_DATA}/doc CACHE PATH "Recommended directory to install documentation into.")
-set ( INSTALL_EXAMPLE ${INSTALL_DATA}/example CACHE PATH "Recommended directory to install examples into.")
-set ( INSTALL_TEST ${INSTALL_DATA}/test CACHE PATH "Recommended directory to install tests into.")
-set ( INSTALL_FOO ${INSTALL_DATA}/etc CACHE PATH "Where to install additional files")
-
-# Skipable content, headers, binaries and libraries are always required
-option ( SKIP_TESTING "Do not add tests." OFF)
 option ( SKIP_LUA_WRAPPER "Do not build and install Lua executable wrappers." OFF)
-option ( SKIP_INSTALL_DATA "Skip installing all data." OFF )
-if ( NOT SKIP_INSTALL_DATA )
-  option ( SKIP_INSTALL_DOC "Skip installation of documentation." OFF )  
-  option ( SKIP_INSTALL_EXAMPLE "Skip installation of documentation." OFF )
-  option ( SKIP_INSTALL_TEST "Skip installation of tests." OFF)
-  option ( SKIP_INSTALL_FOO "Skip installation of optional package content." OFF)
-endif ()
 
-# TWEAKS
-# Setting CMAKE to use loose block and search for find modules in source directory
-set ( CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS true )
-set ( CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_MODULE_PATH} )
+# List of (Lua module name, file path) pairs.
+# Used internally by add_lua_test.  Built by install_lua_module.
+set ( _lua_modules )
 
-# In MSVC, prevent warnings that can occur when using standard libraries.
-if ( MSVC )
-	add_definitions ( -D_CRT_SECURE_NO_WARNINGS )
-endif ()
-
-## MACROS
-# Parser macro
-macro ( parse_arguments prefix arg_names option_names)
-  set ( DEFAULT_ARGS )
-  foreach ( arg_name ${arg_names} )
-    set ( ${prefix}_${arg_name} )
-  endforeach ()
-  foreach ( option ${option_names} )
-    set ( ${prefix}_${option} FALSE )
-  endforeach ()
-
-  set ( current_arg_name DEFAULT_ARGS )
-  set ( current_arg_list )
-  foreach ( arg ${ARGN} )            
-    set ( larg_names ${arg_names} )    
-    list ( FIND larg_names "${arg}" is_arg_name )                   
-    if ( is_arg_name GREATER -1 )
-      set ( ${prefix}_${current_arg_name} ${current_arg_list} )
-      set ( current_arg_name ${arg} )
-      set ( current_arg_list )
-    else ()
-      set ( loption_names ${option_names} )    
-      list ( FIND loption_names "${arg}" is_option )            
-      if ( is_option GREATER -1 )
-	     set ( ${prefix}_${arg} TRUE )
-      else ()
-	     set ( current_arg_list ${current_arg_list} ${arg} )
-      endif ()
-    endif ()
-  endforeach ()
-  set ( ${prefix}_${current_arg_name} ${current_arg_list} )
-endmacro ()
-
-# INSTALL_LUA_EXECUTABLE ( target source )
+# install_lua_executable ( target source )
 # Automatically generate a binary wrapper for lua application and install it
 # The wrapper and the source of the application will be placed into /bin
 # If the application source did not have .lua suffix then it will be added
@@ -215,120 +152,7 @@ return status\;
   install ( PROGRAMS ${_source} DESTINATION ${INSTALL_BIN} RENAME ${_source_name}.lua )
 endmacro ()
 
-# INSTALL_LIBRARY
-# Installs any libraries generated using "add_library" into apropriate places.
-# USE: install_library ( libexpat )
-macro ( install_library )
-  foreach ( _file ${ARGN} )
-    install ( TARGETS ${_file} RUNTIME DESTINATION ${INSTALL_BIN} LIBRARY DESTINATION ${INSTALL_LIB} ARCHIVE DESTINATION ${INSTALL_LIB} )
-  endforeach()
-endmacro ()
-
-# INSTALL_EXECUTABLE
-# Installs any executables generated using "add_executable".
-# USE: install_executable ( lua )
-macro ( install_executable )
-  foreach ( _file ${ARGN} )
-    install ( TARGETS ${_file} RUNTIME DESTINATION ${INSTALL_BIN} )
-  endforeach()
-endmacro ()
-
-# INSTALL_HEADER
-# Install a directories or files into header destination.
-# USE: install_header ( lua.h luaconf.h ) or install_header ( GL )
-# NOTE: If headers need to be installed into subdirectories use the INSTALL command directly
-macro ( install_header )
-  parse_arguments ( _ARG "INTO" "" ${ARGN} )
-  foreach ( _file ${_ARG_DEFAULT_ARGS} )
-    if ( IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_file}" )
-      install ( DIRECTORY ${_file} DESTINATION ${INSTALL_INC}/${_ARG_INTO} )
-    else ()
-      install ( FILES ${_file} DESTINATION ${INSTALL_INC}/${_ARG_INTO} )
-    endif ()
-  endforeach()
-endmacro ()
-
-# INSTALL_DATA ( files/directories )
-# This installs additional data files or directories.
-# USE: install_data ( extra data.dat )
-macro ( install_data )
-  if ( NOT SKIP_INSTALL_DATA )
-    parse_arguments ( _ARG "INTO" "" ${ARGN} )
-    foreach ( _file ${_ARG_DEFAULT_ARGS} )
-      if ( IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_file}" )
-        install ( DIRECTORY ${_file} DESTINATION ${INSTALL_DATA}/${_ARG_INTO} )
-      else ()
-        install ( FILES ${_file} DESTINATION ${INSTALL_DATA}/${_ARG_INTO} )
-      endif ()
-    endforeach()
-  endif()
-endmacro ()
-
-# INSTALL_DOC ( files/directories )
-# This installs documentation content
-# USE: install_doc ( doc/ )
-macro ( install_doc )
-  if ( NOT SKIP_INSTALL_DATA AND NOT SKIP_INSTALL_DOC )
-    parse_arguments ( _ARG "INTO" "" ${ARGN} )
-    foreach ( _file ${_ARG_DEFAULT_ARGS} )
-      if ( IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_file}" )
-        install ( DIRECTORY ${_file} DESTINATION ${INSTALL_DOC}/${_ARG_INTO} )
-      else ()
-        install ( FILES ${_file} DESTINATION ${INSTALL_DOC}/${_ARG_INTO} )
-      endif ()
-    endforeach()
-  endif()
-endmacro ()
-
-# INSTALL_EXAMPLE ( files/directories )
-# This installs additional data
-# USE: install_example ( examples/ exampleA.lua )
-macro ( install_example )
-  if ( NOT SKIP_INSTALL_DATA AND NOT SKIP_INSTALL_EXAMPLE )
-    parse_arguments ( _ARG "INTO" "" ${ARGN} )
-    foreach ( _file ${_ARG_DEFAULT_ARGS} )
-      if ( IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_file}" )
-        install ( DIRECTORY ${_file} DESTINATION ${INSTALL_EXAMPLE}/${_ARG_INTO} )
-      else ()
-        install ( FILES ${_file} DESTINATION ${INSTALL_EXAMPLE}/${_ARG_INTO} )
-      endif ()
-    endforeach()
-  endif()
-endmacro ()
-
-# INSTALL_TEST ( files/directories )
-# This installs tests
-# USE: install_example ( examples/ exampleA.lua )
-macro ( install_test )
-  if ( NOT SKIP_INSTALL_DATA AND NOT SKIP_INSTALL_TEST )
-    parse_arguments ( _ARG "INTO" "" ${ARGN} )
-    foreach ( _file ${_ARG_DEFAULT_ARGS} )
-      if ( IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_file}" )
-        install ( DIRECTORY ${_file} DESTINATION ${INSTALL_TEST}/${_ARG_INTO} )
-      else ()
-        install ( FILES ${_file} DESTINATION ${INSTALL_TEST}/${_ARG_INTO} )
-      endif ()
-    endforeach()
-  endif()
-endmacro ()
-
-# INSTALL_FOO ( files/directories )
-# This installs optional content
-# USE: install_foo ( examples/ exampleA.lua )
-macro ( install_foo )
-  if ( NOT SKIP_INSTALL_DATA AND NOT SKIP_INSTALL_FOO )
-    parse_arguments ( _ARG "INTO" "" ${ARGN} )
-    foreach ( _file ${_ARG_DEFAULT_ARGS} )
-      if ( IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_file}" )
-        install ( DIRECTORY ${_file} DESTINATION ${INSTALL_FOO}/${_ARG_INTO} )
-      else ()
-        install ( FILES ${_file} DESTINATION ${INSTALL_FOO}/${_ARG_INTO} )
-      endif ()
-    endforeach()
-  endif()
-endmacro ()
-
-# INSTALL_LUA_MODULE
+# install_lua_module
 # This macro installs a lua source module into destination given by lua require syntax.
 # Binary modules are also supported where this funcion takes sources and libraries to compile separated by LINK keyword
 # USE: install_lua_module ( socket.http src/http.lua )
@@ -344,9 +168,10 @@ macro (install_lua_module _name )
   parse_arguments ( _MODULE "LINK" "" ${ARGN} )
   get_filename_component ( _ext ${ARGV1} EXT )
   if ( _ext STREQUAL ".lua" )
-      get_filename_component ( _path ${_lua_module} PATH )
-      get_filename_component ( _filename ${_lua_module} NAME )
-      install ( FILES ${ARGV1} DESTINATION ${INSTALL_LMOD}/${_path} RENAME ${_filename} )
+     get_filename_component ( _path ${_lua_module} PATH )
+     get_filename_component ( _filename ${_lua_module} NAME )
+     install ( FILES ${ARGV1} DESTINATION ${INSTALL_LMOD}/${_path} RENAME ${_filename} )
+     list ( APPEND _lua_modules "${_name}" "${CMAKE_CURRENT_SOURCE_DIR}/${ARGV1}" )
   else ()
      enable_language ( C )
      get_filename_component ( _module_name ${_bin_module} NAME_WE )
@@ -360,14 +185,33 @@ macro (install_lua_module _name )
      set_target_properties ( ${_target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${_module_path}" PREFIX "" OUTPUT_NAME "${_module_name}" )
      
      install ( TARGETS ${_target} DESTINATION ${INSTALL_CMOD}/${_module_path})
+     list ( APPEND _lua_modules "${_name}" "${CMAKE_CURRENT_BINARY_DIR}/\${CMAKE_CFG_INTDIR}/${_bin_module}" )
   endif ()
 endmacro ()
 
-# ADD_LUA_TEST
+# Builds string representing Lua table mapping Lua modules names to file
+# paths.  Used internally.
+macro ( _make_module_table _outvar )
+	set ( ${_outvar} )
+	list ( LENGTH _lua_modules _n )
+	foreach ( _i RANGE 1 ${_n} 2 )
+		list ( GET _lua_modules ${_i} _path )
+		math ( EXPR _ii ${_i}-1 )
+		list ( GET _lua_modules ${_ii} _name )
+		set ( ${_outvar} "${_table}  ['${_name}'] = '${_path}'\;\n")
+	endforeach ()
+	set ( ${_outvar}
+"local modules = {
+${_table}}" )
+endmacro ()
+
+# add_lua_test ( _testfile [ WORKING_DIRECTORY _working_dir ] )
 # Runs Lua script `_testfile` under CTest tester.
 # Optional named argument `WORKING_DIRECTORY` is current working directory to run test under
 # (defaults to ${CMAKE_CURRENT_BINARY_DIR}).
 # Both paths, if relative, are relative to ${CMAKE_CURRENT_SOURCE_DIR}.
+# Any modules previously defined with install_lua_module are automatically preloaded
+# (via package.preload) prior to running the test script.
 # Under LuaDist, set test=true in config.lua to enable testing.
 # USE: add_lua_test ( test/test1.lua [args...] [WORKING_DIRECTORY dir])
 macro ( add_lua_test _testfile )
@@ -380,12 +224,27 @@ macro ( add_lua_test _testfile )
 		get_filename_component ( TESTFILEBASE ${_testfile} NAME_WE )
 
 		# Write wrapper script.
+		# Note: One simple way to allow the script to find modules is
+		# to just put them in package.preload.
 		set ( TESTWRAPPER ${CMAKE_CURRENT_BINARY_DIR}/${TESTFILENAME} )
+		_make_module_table ( _table )
 		set ( TESTWRAPPERSOURCE
 "local configuration = ...
-local sodir = '${CMAKE_CURRENT_BINARY_DIR}' .. (configuration == '' and '' or '/' .. configuration)
-package.path  = sodir .. '/?.lua\;' .. sodir .. '/?.lua\;' .. package.path
-package.cpath = sodir .. '/?.so\;'  .. sodir .. '/?.dll\;' .. package.cpath
+${_table}
+local function preload_modules(modules)
+  for name, path in pairs(modules) do
+    if path:match'%.lua' then
+      package.preload[name] = assert(loadfile(path))
+    else
+      local name = name:gsub('.*%-', '') -- remove any hyphen prefix
+      local symbol = 'luaopen_' .. name:gsub('%.', '_')
+          --improve: generalize to support all-in-one loader?
+      local path = path:gsub('%$%{CMAKE_CFG_INTDIR%}', configuration or '')
+      package.preload[name] = assert(package.loadlib(path, symbol))
+    end
+  end
+end
+preload_modules(modules)
 arg[0] = '${TESTFILEABS}'
 table.remove(arg, 1)
 return assert(loadfile '${TESTFILEABS}')(unpack(arg))
@@ -401,6 +260,7 @@ return assert(loadfile '${TESTFILEABS}')(unpack(arg))
 	endif ()
 	# see also http://gdcm.svn.sourceforge.net/viewvc/gdcm/Sandbox/CMakeModules/UsePythonTest.cmake
 endmacro ()
+
 
 # Converts Lua source file `_source` to binary string embedded in C source
 # file `_target`.  Optionally compiles Lua source to byte code (not available
